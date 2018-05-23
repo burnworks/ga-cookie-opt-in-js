64cba357653d1b17ede8d1449871dece28e7b32f
burnworks <yoshiki.kato@gmail.com>
2018-05-24 00:03:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ga-cookie-opt-in-js
+
+Cookie 使用の同意を得てから Google Analytics のトラッキングを有効にする JavaScript のサンプル
+
+## 詳しい解説
+
+- <a href="https://hyper-text.org/archives/2018/05/google_analytics_cookie_opt_in.shtml" target="_blank">Cookie 使用の同意を得てから Google Analytics のトラッキングを有効にする - WWW Watch</a>
+
+## 使い方
+
+- src/css/ga-cookie-opt-in.min.css をサーバの任意のディレクトリに設置
+- src/js/ga-cookie-opt-in.js 内の「プロパティ ID」を修正、ga-cookie-opt-in.min.css のパスを設置した場所に修正の上、サーバの任意のディレクトリに設置
+- src/js/ga-cookie-opt-in.js を body 要素内、もしくは head 要素内で読み込み
+- 必要に応じて .js ファイルや .css ファイルをカスタマイズしてください
+
+## 注意事項
+
+本スクリプトはサンプルです。本番環境で使用した際に正しく動作しないなどといった場合でも一切責任は負いませんのであらかじめご了承ください。
---
+++ src/js/ga-cookie-opt-in.js
@@ -0,0 +1,77 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  'use strict';
+  //localStorage から ga_cookie_opt_in の値を取得
+  var cookieOptin = localStorage.getItem('ga_cookie_opt_in');
+
+  //ga_cookie_opt_in = no なら Google Analytics を無効に
+  if(cookieOptin == 'no') {
+    console.log('ga_cookie_opt_in = no / ga-disable = true');
+    //Disable Google Analytics（UA-XXXX-Y の部分は正しいものに書き換えてください）
+    window['ga-disable-UA-XXXX-Y'] = true;
+
+  //ga_cookie_opt_in = yes なら Google Analytics トラッキングコードを発行
+  } else if(cookieOptin == 'yes') {
+    console.log('ga_cookie_opt_in = yes');
+    //Enable Google Analytics（UA-XXXX-Y の部分は正しいものに書き換えてください）
+    window['ga-disable-UA-XXXX-Y'] = false;
+
+  //ga_cookie_opt_in に値がない場合は一旦 Google Analytics を無効にして通知を表示
+  } else {
+    console.log('ga_cookie_opt_in = null');
+    //Disable Google Analytics（UA-XXXX-Y の部分は正しいものに書き換えてください）
+    window['ga-disable-UA-XXXX-Y'] = true;
+
+    // ↓Google Web Fonts から Material Icons を読み込み（すでに別で読み込んでいる場合は削除可）↓
+    var iconcss = document.createElement('link');
+        iconcss.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+        iconcss.setAttribute('rel', 'stylesheet');
+        document.head.appendChild(iconcss);
+    // ↑Google Web Fonts から Material Icons を読み込み（すでに別で読み込んでいる場合は削除可）↑
+    // 通知バーのスタイル（パスは必要に応じて変更してください）
+    var acceptcss = document.createElement('link');
+        acceptcss.href = 'css/ga-cookie-opt-in.min.css';
+        acceptcss.setAttribute('rel', 'stylesheet');
+        document.head.appendChild(acceptcss);
+
+    //通知の表示（テキストの内容やプライバシーポリシーへのリンクは必要に応じて変更してください）
+    var accept = document.createElement('div');
+        accept.setAttribute('class', 'module-ga-cookie-accept-bar');
+        accept.setAttribute('id', 'name-ga-cookie-accept-bar');
+        accept.innerHTML = '<p><i class="material-icons" aria-hidden="true">warning</i>このサイトでは Google アナリティクスの Cookie（クッキー）を使用して、ユーザーのWebサイト閲覧データを記録しています。<a href="./"><i class="material-icons" aria-hidden="true">chevron_right</i>プライバシーポリシーを確認</a></p><p><button id="name-ga-cookie-accept-btn" class="module-ga-cookie-accept-btn"><i class="material-icons" aria-hidden="true">check</i>同意して Cookie を受け入れる</button> <button id="name-ga-cookie-deny-btn" class="module-ga-cookie-accept-btn module-ga-cookie-deny-btn"><i class="material-icons" aria-hidden="true">close</i>同意しない</button></p>';
+        document.body.appendChild(accept);
+  }
+
+  //各ボタンの取得
+  var acceptBtn = document.getElementById('name-ga-cookie-accept-btn');
+  var denyBtn   = document.getElementById('name-ga-cookie-deny-btn');
+  var resetBtn  = document.getElementById('name-ga-cookie-reset-btn');
+
+  //「同意する」ボタンのクリックでオプトイン（ga_cookie_opt_in = yes）
+  if(acceptBtn) {
+    acceptBtn.onclick = function() {
+      localStorage.setItem('ga_cookie_opt_in','yes');
+      document.getElementById('name-ga-cookie-accept-bar').classList.add('state-remove');
+      //CSS Animation が完了するまで待機するための力技……
+      window.setTimeout('window.location.reload(false)', 500);
+    };
+  }
+
+  //「同意しない」ボタンのクリックでオプトアウト（ga_cookie_opt_in = no）
+  if(denyBtn) {
+    denyBtn.onclick = function() {
+      localStorage.setItem('ga_cookie_opt_in','no');
+      document.getElementById('name-ga-cookie-accept-bar').classList.add('state-remove');
+      window.setTimeout('window.location.reload(false)', 500);
+    };
+  }
+
+  //「設定をリセット」ボタンのクリックで ga_cookie_opt_in の値を削除
+  if(resetBtn) {
+    resetBtn.onclick = function() {
+      localStorage.removeItem('ga_cookie_opt_in');
+      location.reload(false);
+    };
+  }
+
+});
